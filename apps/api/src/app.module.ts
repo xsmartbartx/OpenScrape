7f6d818bc23ab3b2f315793b2ea0d3059b5c5c70
@MiejskiSurfer <73461850+xsmartbartx@users.noreply.github.com>
2026-09-01 23:41:54 +0200

    app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { HealthController } from './health.controller';
+import { RobotsController } from './robots.controller';
 
 @Module({
-  controllers: [AppController, HealthController],
+  controllers: [AppController, HealthController, RobotsController],
 })
 export class AppModule {}
